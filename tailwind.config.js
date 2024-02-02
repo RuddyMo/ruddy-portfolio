861486c3dba21b7ba36f1801991d76cee78cb4e2
RuddyMo <ruddymorel10@gmail.com>
2024-02-02 22:41:24 +0100

    create project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+ ],
+  css: ['~/assets/main.css'],
+  theme: {
+    fontFamily: {
+    },
+    extend: {
+      colors: {
+      },
+      textColor: {
+      },
+    },
+  },
+  plugins: [],
+};
